0ab7c5ab64aa23400753875257e2e375a8edf1e0
Fernier <fernier.paul@gmail.com>
2025-09-20 10:13:08 +0200

    back

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    message: "🚀 API is running on Vercel!",
+    endpoints: {
+      signup: "/users/signup",
+      signin: "/users/signin",
+      weather: "/weather",
+    }
+  });
+});
+
+
+
+
+
 module.exports = router;
